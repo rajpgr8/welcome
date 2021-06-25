938905d9377fefae0387a431c975180246d83ae0
rajpgr8 <rajp.gr8@gmail.com>
2021-06-25 15:49:08 +0100

    Update test_app2.py

--- tests/test_app2.py
+++ tests/test_app2.py
@@ -5,6 +5,10 @@ def test_2ndTest_1():
   print("2ndTest-1")
   assert 1 == 2
     
-def test_2ndTest_2():
+def test_2ndTest_2(setup):
   print("1stTest-2")
+  assert 1 == 1
+  
+def test_2ndTest_3():
+  print("1stTest-3")
   assert 1 == 1
