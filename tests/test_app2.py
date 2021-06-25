088a07bccba3d2f0d96d8e0ae6c5556696fa1824
rajpgr8 <rajp.gr8@gmail.com>
2021-06-25 15:32:19 +0100

    Update test_app2.py

--- tests/test_app2.py
+++ tests/test_app2.py
@@ -1,6 +1,10 @@
   
 import pytest
 
-def test_2ndTest():
-  print("2ndTest")
+def test_2ndTest_1():
+  print("2ndTest-1")
   assert 1 == 2
+    
+def test_2ndTest_2():
+  print("1stTest-2")
+  assert 1 == 1
