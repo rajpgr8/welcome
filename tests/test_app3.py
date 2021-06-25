d6d889a2cbd3194cabbde54d8fb328e99c0e2b9d
rajpgr8 <rajp.gr8@gmail.com>
2021-06-25 15:58:35 +0100

    Update test_app3.py

--- tests/test_app3.py
+++ tests/test_app3.py
@@ -2,9 +2,9 @@ import pytest
 
 @pytest.mark.usefixtures("setup")
 class TestExample:
-  def test_fixtureDemo_1():
+  def test_fixtureDemo_1(self):
     print("test_fixtureDemo_1")
     
-  def test_fixtureDemo_2():
+  def test_fixtureDemo_2(self):
     print("test_fixtureDemo_2")
   
