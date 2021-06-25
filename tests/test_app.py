047059f1c96ea6a9f7901d25fc3a565fc48d998b
rajpgr8 <rajp.gr8@gmail.com>
2021-06-25 15:23:28 +0100

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,13 @@
 import pytest
 
-def test_1stTest():
-  print("1stTest")
+def test_1stTest_1():
+  print("1stTest-1")
+  assert 1 == 1
+  
+def test_1stTest_2():
+  print("1stTest-2")
+  assert 1 == 1
+    
+def test_1stTest_3():
+  print("1stTest-3")
   assert 1 == 1
