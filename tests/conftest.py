c7ec048c46b4e1a013a23d611d7ad9ef447f5ed7
rajpgr8 <rajp.gr8@gmail.com>
2021-06-25 16:55:50 +0100

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,3 +4,7 @@ import pytest
 def setup():
   print("setup: ***setup() will be executed before any other tests (dependent on this fixture) run***")
 
+@pytest.fixture()
+def loadSomeData():
+  print("loadSomeData: ***loadSomeData()***")
+  return ["Joe", 3, 700]
