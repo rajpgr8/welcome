a87d570c3d633b0980350abb001c3cf296b0a8a0
rajpgr8 <rajp.gr8@gmail.com>
2021-06-25 17:34:18 +0100

    Update test_app3.py

--- tests/test_app3.py
+++ tests/test_app3.py
@@ -7,4 +7,10 @@ class TestExample:
     
   def test_fixtureDemo_2(self):
     print("test_fixtureDemo_2")
+
+@pytest.mark.usefixtures("loadSomeData")
+class TestExample_2:
+  def test_fixtureDemo_3(self, loadSomeData):
+    print("test_fixtureDemo_2")
+    print(loadSomeData)
   
