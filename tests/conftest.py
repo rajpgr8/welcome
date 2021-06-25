f4b20a3a028d54ee922a90cbc002f776efbc4446
rajpgr8 <rajp.gr8@gmail.com>
2021-06-25 15:42:27 +0100

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,5 +2,5 @@ import pytest
 
 @pytest.fixture()
 def setup():
-  print("setup: should be exceuted before other fixture test run")
+  print("setup: ***should be exceuted this setup() before other fixture test run***")
 
