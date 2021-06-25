28059f91bc1bbcf90489e06414a7b3577621c236
rajpgr8 <rajp.gr8@gmail.com>
2021-06-25 17:44:48 +0100

    Update test_app3.py

--- tests/test_app3.py
+++ tests/test_app3.py
@@ -9,8 +9,15 @@ class TestExample:
     print("test_fixtureDemo_2")
 
 @pytest.mark.usefixtures("loadSomeData")
-class TestExample_2:
-  def test_fixtureDemo_3(self, loadSomeData):
-    print("test_fixtureDemo_2")
+class TestExample_LoadData:
+  def test_loadData_check(self, loadSomeData):
+    print("test_loadData_check")
     print(loadSomeData)
   
+
+  
+@pytest.mark.usefixtures("parameterizedOption")
+class TestExample_parameterizedOptionCheck:
+  def test_parameterizedOption_check(self, parameterizedOption):
+    print("test_parameterizedOption_check")
+    print(parameterizedOption)
